42d08e4dfe40da3ccb3e467751758a2f3d68a2ed
Justin Phillips <justinphil02@yahoo.com>
2019-11-22 18:32:47 +0000

    Add CLI controller loop, working on processing API response

--- lib/API.rb
+++ lib/API.rb
@@ -1,7 +1,8 @@
 class API  
 #key a9ca6c61110b8a16ee8dd7a8d661ed33  
   def artist 
-  artist_info = open('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json') 
+  artist_info = open('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json').first
+  binding.pry 
   end 
   
 end 
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,7 +1,17 @@
 class CLI 
-  
+
   def call 
-    API.new.artist 
-  end 
+    
+    puts "Welcome to Your Favorite Artist!"
+    input = ""
+    while input != 'exit'
+    puts "Who is your favorite artist?"
+    puts "Enter your favorite artist or exit"
+    input = gets.chomp 
+    
+    puts input
+   end #end of program loop
+   
+  end #end of call method 
   
-end 
+end #end of class 
