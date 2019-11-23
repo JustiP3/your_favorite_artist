5d96f582b8842a650c71eb240f440777da7da71a
Justin Phillips <justinphil02@yahoo.com>
2019-11-23 23:57:58 +0000

    add test for artist that cannot be found

--- lib/your_favorite_artist/API.rb
+++ lib/your_favorite_artist/API.rb
@@ -3,13 +3,19 @@ class API
 
 
   def self.get_artist_info(artist)
-  
+#  artist_name_not_on_lastfm = "somesuperobscurename"
   link = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{artist}&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json"
   hash = HTTParty.get(link).parsed_response 
-  artist_info = {
-    :name => hash["artist"]["name"],
-    :bio => hash["artist"]["bio"]["summary"]
-  }
+  
+  if hash["error"] == 6
+    artist_info = hash 
+  else 
+    artist_info = {
+      :name => hash["artist"]["name"],
+      :bio => hash["artist"]["bio"]["summary"]
+    }
+  end 
+  
   artist_info
   end 
   
--- lib/your_favorite_artist/artist.rb
+++ lib/your_favorite_artist/artist.rb
@@ -7,10 +7,14 @@ class Artist
   
   def get_info
     temp_hash = API.get_artist_info(self.name)
+    if temp_hash["error"]
+      puts temp_hash["message"]
+    else 
     self.name = temp_hash[:name]
     self.bio = temp_hash[:bio]
     self.short_bio = self.bio.slice(0,50)
     self.short_bio << "..."
+    end 
     binding.pry 
   end 
   
