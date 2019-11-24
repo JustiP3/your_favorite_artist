2449c4663cd2d166d3590ebff8649bc554339df3
Justin Phillips <justinphil02@yahoo.com>
2019-11-24 20:43:50 +0000

    build CLI loop to gather additional info after finding artist.

--- lib/your_favorite_artist/CLI.rb
+++ lib/your_favorite_artist/CLI.rb
@@ -7,23 +7,26 @@ attr_accessor :input
   end 
   
   def call 
-    
+    puts "--------------------------------"
     puts "Welcome to Your Favorite Artist!"
+    puts "--------------------------------"
     
     while self.input != 'exit'
+      get_user_input_artist
+      if input != 'exit'
+        fav_artist = Artist.new(input)
+        print_artist_info(fav_artist)
+      end 
+      exit_prompt
+      if input != 'exit'
+        get_user_input_details
+      end 
     
-    get_user_input_artist
-    if input != 'exit'
-      fav_artist = Artist.new(input)
-      print_artist_info(fav_artist)
-    end 
-    
-   if input != 'exit'
-     input = exit_prompt
-   end 
-   
+      if input != 'exit'
+        exit_prompt
+      end 
    end #end of program loop
-   
+   be_polite_say_goodbye 
   end #end of call method 
   
   
@@ -36,23 +39,47 @@ attr_accessor :input
     return input 
   end #end of method get_user_input 
   
-  def exit_prompt
-    puts "Would you like to continue?"
-    puts "Enter 'exit' to quit or press 'enter' to continue"
-    self.input = gets.chomp 
-    
+  def get_user_input_details
+  self.input = ""
+  until input.length > 0 
+    puts "1. See related artists"
+    puts "2. See top albums"
+    puts "3. See top songs"
+    puts "What would you like to see?"
+    puts "Enter 1-3 or 'exit' to quit"
+    input = gets.chomp 
+    if input != 1 || input != 2 || input != 3
+      input = ""
+      puts "That was an invalid input, please try again"
+    end 
+    if input == 'exit'
+      return 
+    end 
   end 
+  end # end of method  
   
   def print_artist_info(artist)
     artist.get_info
       if artist.error == false 
-        puts "Your favorite artist goes by the name #{artist.name}"
+        puts "You picked #{artist.name}!"
         puts "Artist Bio:"
         puts artist.short_bio
       else 
         puts artist.error_message 
       end 
   end 
+  
+    def exit_prompt
+    puts "Would you like to continue?"
+    puts "Enter 'exit' to quit or press 'enter' to continue"
+    self.input = gets.chomp 
+  end 
+  
+  def be_polite_say_goodbye
+    puts "Thank you for stopping by!"
+    puts "See you next time, friend."
+  end 
+  
   
   
 end #end of class 
