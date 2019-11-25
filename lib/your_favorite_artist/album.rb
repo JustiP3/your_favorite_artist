c501901d1d3f78190a3779ce71002bbeba6bed9d
Justin Phillips <justinphil02@yahoo.com>
2019-11-25 13:32:03 +0000

    added methods to album class

--- lib/your_favorite_artist/album.rb
+++ lib/your_favorite_artist/album.rb
@@ -1,3 +1,19 @@
+#This class only interacts with the artist class
 class Album
+  @@all = []
+  
+  attr_accessor :name, :rank, :play_count
+  
+  def initialize (name, rank, play_count)
+    @name = name
+    @rank = rank
+    @play_count = play_count
+    self << @@all 
+  end 
+  
+  self.sort
+    @@all.sort {|a1, a2| a1.rank <=> a2.rank}
+  end 
+  
   
 end 
