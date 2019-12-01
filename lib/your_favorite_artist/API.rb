c03025f215231e7d98aa1cf7bf55b6d07f1552bc
Justin Phillips <justinphil02@yahoo.com>
2019-12-01 19:52:22 +0000

    API class is formatted nicely and pry is no longer required

--- lib/your_favorite_artist/API.rb
+++ lib/your_favorite_artist/API.rb
@@ -2,24 +2,25 @@ class API
 #key a9ca6c61110b8a16ee8dd7a8d661ed33
 
   def self.get_artist_info(artist)
-  link = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{artist}&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json"
-  hash = HTTParty.get(link).parsed_response 
-  if hash["error"] == 6 
-    artist_info = hash 
-  else 
-    related_artists = []
-    hash["artist"]["similar"]["artist"].each {|artist_hash| related_artists.push(artist_hash["name"])}
-    artist_info = {
-      :name => hash["artist"]["name"],
-      :similar => related_artists
-    }
+    link = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{artist}&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json"
+    hash = HTTParty.get(link).parsed_response 
+    if hash["error"] == 6 
+      artist_info = hash 
+    else 
+      related_artists = []
+      hash["artist"]["similar"]["artist"].each {|artist_hash| related_artists.push(artist_hash["name"])}
+      artist_info = {
+        :name => hash["artist"]["name"],
+        :similar => related_artists
+      }
+    
       if hash["artist"]["bio"]["summary"].include?("https://www.last.fm/music/+noredirect")
         artist_info[:bio] = "Sorry, this artist does not have a bio."
       else 
         artist_info[:bio] = hash["artist"]["bio"]["summary"]
       end 
-  end 
-  artist_info
+    end 
+    artist_info
   end 
   
   def self.get_top_albums(artist)
@@ -33,12 +34,12 @@ class API
     end 
     
     short_list.each.with_index(1) {|album, i| artist.create_album(album, i)}
-
   end 
   
   def self.get_album_info(artist, album)
     link =  "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&artist=#{artist.name}&album=#{album.name}&format=json"
     hash = HTTParty.get(link).parsed_response
+    
     hash["album"]["tracks"]["track"].each {|track| album.track_list.push(track["name"])}
     album.track_list 
   end 
@@ -47,6 +48,7 @@ class API
     link = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=#{artist.name}&api_key=a9ca6c61110b8a16ee8dd7a8d661ed33&format=json"
     hash = HTTParty.get(link).parsed_response
     top_tracks = []
+    
     hash["toptracks"]["track"].each {|song| top_tracks.push(song["name"])}
     top_tracks.slice(0..4)
   end 
